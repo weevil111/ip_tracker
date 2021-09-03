c74c3c3d79fc70d946b7427aea1f7c2feece749d
weevil <abhinavparagmishra23@gmail.com>
2021-09-03 21:43:33 +0530

    Integrate map using leaflet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/Header/Header";
-
+import Map from "./components/Map/Map"
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      
+      <Map></Map>
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import L from "leaflet"
+import icon from "../../assets/icon-location.svg"
 
 function Map() {
+  const customMarker = new L.Icon({
+    iconUrl: icon,
+    iconRetinaUrl: icon,
+    iconAnchor: null,
+    popupAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(35, 45),
+    className: ''
+  });
   const position = [51.505, -0.09]
   return (
     <MapContainer
       center={position}
       zoom={13}
-      style={{height: "63vh"}}
+      style={{ height: "64vh" }}
     >
       <TileLayer
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=9AQA3kM6G5K9AzVv0GqW"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[51.505, -0.09]} icon={customMarker}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
